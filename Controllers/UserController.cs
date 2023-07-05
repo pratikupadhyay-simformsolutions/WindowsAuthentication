ab55fc474dbd8aca500eb050308f286c5914a450
Pratik Upadhyay <pratik.u@simformsolutions.com>
2023-07-05 21:22:20 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WindowsAuthentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetCurrentUser")]
+        public string GetCurrentUser()
+        {
+            return HttpContext.User.Identity.Name;
+        }
+    }
+}
